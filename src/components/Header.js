0641520f41e9a9e0ea6c33b5d55979cd4f92ad13
abhinandanmishra1 <abhinandanmishra360@gmail.com>
2023-03-09 00:18:18 +0530

    fix: link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import Logo from '../assets/logo.svg';
-
 const Header = () => {
+  const openLinkedin = () => {
+    window.open('https://www.linkedin.com/in/abhinandanmishra1/', '_blank');
+  }
+
   return <header className='py-8'>
     <div className="container mx-auto">
         <div className='flex justify-between items-center'>
@@ -10,7 +12,7 @@ const Header = () => {
             <span className='logo'>Abhinandan</span> Mishra
           </a>
 
-          <button className='btn btn-sm'>Hire Me</button>
+          <button className='btn btn-sm' onClick={openLinkedin}>Hire Me</button>
         </div>
     </div>
   </header>
